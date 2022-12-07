87ef4b5f952f342e85bae652aa5344895bffff73
RileyK646 <RileyKing646@gmail.com>
2022-12-07 15:33:00 -0500

    where is easy

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,4 +1,10 @@
 USE employees;
-SELECT first_name FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
-SELECT  last_name FROM employees WHERE last_name LIKE 'E%';
+SELECT first_name FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+AND gender IN ('M');
+SELECT  last_name FROM employees WHERE last_name LIKE 'E%'
+OR last_name LIKE '%E';
 SELECT last_name FROM employees WHERE last_name LIKE '%q%';
+SELECT  last_name FROM employees WHERE last_name LIKE 'E%'
+                                    AND last_name LIKE '%E';
+SELECT last_name FROM employees WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%';
